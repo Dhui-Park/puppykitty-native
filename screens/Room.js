d7de48a3b71c938abd753f9d1595f653c70cfcb7
Dhui-Park <hasong925@gmail.com>
2023-03-02 19:48:58 +0900

    Subscriptions part Four

--- screens/Room.js
+++ screens/Room.js
@@ -169,6 +169,12 @@ export default function Room({ route, navigation }) {
         id: `Room:${route.params.id}`,
         fields: {
           messages(prev) {
+            const existingMessage = prev.find(
+              (aMessage) => aMessage.__ref === messageFragment.__ref
+            );
+            if (existingMessage) {
+              return prev;
+            }
             return [...prev, messageFragment];
           },
         },
